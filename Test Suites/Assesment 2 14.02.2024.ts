debe593607d42ca06b8b10bf4df052ebe121bf37
ARCHANA R <archstesting@gmail.com>
2024-06-14 15:06:45 +0530

    Add files via upload

---
+++ Test Suites/Assesment 2 14.02.2024.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Assesment 2 14.02.2024</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f5869fc4-6239-45f2-945c-18a4d5453562</testSuiteGuid>
+   <testCaseLink>
+      <guid>388d68a9-7acb-4fc5-8bfa-3742bac469ca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Assesment 2 14.06.2024</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
